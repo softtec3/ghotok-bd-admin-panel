9f5c8d702535ead6793f5cebb8a4978e7627c432
Md Tahmid Alam <mdtahmidalam122@gmail.com>
2025-11-11 09:51:25 +0600

    update is logged in logic

--- php/change_bio_status.php
+++ php/change_bio_status.php
@@ -1,4 +1,5 @@
 <?php
+require_once("is_logged_in.php");
 require_once("db_connect.php");
 try {
     // active
--- php/is_logged_in.php
+++ php/is_logged_in.php
@@ -2,4 +2,5 @@
 session_start();
 if (!isset($_SESSION) || !isset($_SESSION["admin_id"]) || empty($_SESSION) || $_SESSION["admin_id"] == "" || empty($_SESSION["admin_id"])) {
     header("Location: ./");
+    exit();
 }
